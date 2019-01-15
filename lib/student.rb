06b7af12b0acadd4a2397da31d4d68a353c9d768
Travis Williamson <travywill@gmail.com>
2019-01-15 16:39:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -113,7 +113,7 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, number_of_students).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
