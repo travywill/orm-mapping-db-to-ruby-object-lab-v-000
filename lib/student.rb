48a71e7d335b77a38f1a172850aabbd46e5769ae
Travis Williamson <travywill@gmail.com>
2019-01-15 16:39:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,7 +106,16 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    #
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, number_of_students).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.all_students_in_grade_X
