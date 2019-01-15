27ef8c361a377e4aec6e1c071ee3cd848cec517e
Travis Williamson <travywill@gmail.com>
2019-01-15 17:10:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -127,7 +127,11 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X
-    #
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
   end
 end
