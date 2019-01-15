737f6343fe92609b139b554887187741b242e7b8
Travis Williamson <travywill@gmail.com>
2019-01-15 17:01:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -119,12 +119,12 @@ class Student
       SELECT *
       FROM students
       WHERE grade = 10
-      LIMIT 2
+      LIMIT 1
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
   def self.all_students_in_grade_X
