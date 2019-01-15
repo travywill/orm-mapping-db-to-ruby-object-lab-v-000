3a0ce5ec2cd6f237f1cecae1c821051d055b89d0
Travis Williamson <travywill@gmail.com>
2019-01-15 16:48:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def initialize(name, grade, id = nil)
-    @id = id
-    @name = name
-    @grade = grade
-  end
+  #def initialize(name, grade, id = nil)
+    #@id = id
+    #@name = name
+    #@grade = grade
+  #end
 
   def save
     sql = <<-SQL
